6b539a7fecca13af6f58573e88138fb6d6c86e0c
Eriknicklasson <62430975+Eriknicklasson@users.noreply.github.com>
2022-04-23 01:11:00 +0200

    jobbat

--- snake.js
+++ snake.js
@@ -1,18 +1,25 @@
-export const SNAKE_SPEED = 2
+import { getInputDirection } from "./input.js"
+
+
+
+export const SNAKE_SPEED = 4
 const snakeBody = [
-    {x: 10, y: 11},
     {x: 11, y: 11},
-    {x: 12, y: 11}
 ]
+let newsegments = 0;
 
 
 
 export function update()
 {
+    const inputDirection = getInputDirection()
     for(let i = snakeBody.length - 2; i >= 0; i--)
     {
         snakeBody[i + 1] = {...snakeBody[i]}
     }
+
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
  
 }
 
@@ -20,10 +27,27 @@ export function draw(gameBoard)
 {
     snakeBody.forEach(segment => { 
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.x
-        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
   
+}
+
+export function expandSnake(amount)
+{
+    newsegments += amount
+}
+
+export function onSnake(position)
+{
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2)
+{
+    return pos1.x === pos2.x && pos1.y === pos2.y
 }
